5e2aa71126d758fd532aa9035b764e90659f4da3
CoderAmiru <amirumadduma109@gmail.com>
2021-07-20 20:12:30 +1000

    log

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint= Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -33,10 +34,14 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    log6= new Log(230,180,80,PI/2);
+    
     bird = new Bird(100,100);
+    chain= new Chain(bird.body,log6.body)
 
 }
+
+
 
 function draw(){
     background(backgroundImg);
@@ -58,7 +63,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    log6.display();
     bird.display();
     platform1.display();
+    chain.display();
 }
